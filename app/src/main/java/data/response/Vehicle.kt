28dca226ccbf1c073f95984382c6f41d33160016
Milad <Milad.faridnia@gmail.com>
2020-07-27 21:09:53 +0430

    feat: Add vehicle data model

---
+++ app/src/main/java/data/response/Vehicle.kt
@@ -0,0 +1,13 @@
+package data.response
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Vehicle(
+    val bearing: Int,
+    @SerializedName("image_url")
+    val imageUrl: String,
+    val lat: Double,
+    val lng: Double,
+    val type: String
+)
