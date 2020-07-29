d56b91d5d618e5cd20338dca1292df8b66dfa0a3
Milad <Milad.faridnia@gmail.com>
2020-07-29 14:35:53 +0430

    fix: Fix suspend functions in VehicleDao

--- app/src/main/java/com/faridnia/assignment/room/VehicleDao.kt
+++ app/src/main/java/com/faridnia/assignment/room/VehicleDao.kt
@@ -6,13 +6,16 @@ import androidx.room.*
 @Dao
 interface VehicleDao {
     @Query("SELECT * FROM vehicle_table")
+            /*  No need to add suspend keyword because it will automatically handled asynchronous.
+              Since i'm returning a LiveData object, there’s no need to use suspend
+               on this method. In fact, Room won’t even allow it. */
     fun getAll(): LiveData<List<Vehicle>>
 
     @Query("SELECT * FROM vehicle_table WHERE vehicleId IN (:vehicleIds)")
-    fun loadAllByIds(vehicleIds: IntArray): List<Vehicle>
+    suspend fun loadAllByIds(vehicleIds: IntArray): List<Vehicle>
 
     @Query("SELECT * FROM vehicle_table WHERE vehicleId  =:id LIMIT 1")
-    fun findById(id: Int): Vehicle
+    suspend fun findById(id: Int): Vehicle
 
     @Insert
     suspend fun insertAll(vararg vehicles: Vehicle)
